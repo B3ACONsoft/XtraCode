2c1421d6d3d304d64140e622c09284676c70799c
wakeTechN00b <dhjohnson3@my.waketech.edu>
2016-02-28 12:41:15 -0500

    added comments to coachQueries.sql

--- dummyDataDBScripts/coachQueries.sql
+++ dummyDataDBScripts/coachQueries.sql
@@ -1,3 +1,5 @@
+/*Should select all enrollment data asociated with a coach's user_id
+    define("GET_COACH_ENROLLMENT", "NOTHING FOR NOW")*/
 SELECT E.enrollment_id, 
 	   E.user_id, 
        E.player_id,
@@ -8,7 +10,7 @@ SELECT E.enrollment_id,
 FROM
     enrollment E, player P, users U
 WHERE
-	 (U.user_id = 4)
+	 (U.user_id = :coach_user_id)
      	AND
      (P.user_id = U.user_id)
         AND
