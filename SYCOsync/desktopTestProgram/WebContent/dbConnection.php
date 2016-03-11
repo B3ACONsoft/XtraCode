2020afc0c711a8cfb73e46d3e372bee0f84402f6
wakeTechN00b <dhjohnson3@my.waketech.edu>
2016-03-11 18:52:43 -0500

    added more insert querry bindings

--- SYCOsync/desktopTestProgram/WebContent/dbConnection.php
+++ SYCOsync/desktopTestProgram/WebContent/dbConnection.php
@@ -46,22 +46,93 @@ function doInsert($query) {
 		    $error_message\r\n\r\n";
         }    
 }
-
+/**
+ * Binds the values needed to insert a sport
+ */
+function addSportBinding(&$query, $values) {
+	$query->bindParam(':sport_name', $values['sport_name']);
+	
+}
 
 /**
- * Binds the given values to the given prepared statement query.
- * 
+ * Binds the values needed to insert a league
+ */
+function addLeagueBinding(&$query, $values) {
+	$query->bindParam(':league_name', $values['league_name']);
+	$query->bindParam(':sport_id', $values['sport_id']);
+	$query->bindParam(':min_age', $values['min_age']);
+	$query->bindParam(':max_age', $values['max_age']);
+	$query->bindParam(':start_date', $values['start_date']);
+	$query->bindParam(':end_date', $values['end_date']);
+}
+
+/**
+ * Binds the values needed to insert a user.
+ *
  * @param unknown $query
  * @param unknown $values
  */
-function insertUserBinding(&$query, $values) {
-    $query->bindParam(':fname', $values['fname']);
-    $query->bindParam(':lname', $values['lname']);
-    $query->bindParam(':phone', $values['phone']);
-    $query->bindParam(':emergency', $values['emergency']);
-    $query->bindParam(':email', $values['email']);
-    $query->bindParam(':user_type', $values['user_type']);
-    $query->bindParam(':password', $values['password']);
+function addUserBinding(&$query, $values) {
+	$query->bindParam(':fname', $values['fname']);
+	$query->bindParam(':lname', $values['lname']);
+	$query->bindParam(':phone', $values['phone']);
+	$query->bindParam(':emergency', $values['emergency']);
+	$query->bindParam(':email', $values['email']);
+	$query->bindParam(':user_type', $values['user_type']);
+	$query->bindParam(':password', $values['password']);
+}
+
+/**
+ * Binds the values needed to insert a team
+ */
+function addTeamBinding(&$query, $values) {
+	$query->bindParam(':league_id', $values['league_id']);
+	$query->bindParam(':team_name', $values['team_name']);
+	$query->bindParam(':user_id', $values['user_id']);
+}
+
+/**
+ * Binds the values needed to insert an enrollment record.
+ *
+ * @param unknown $query
+ * @param unknown $values
+ */
+function addEnrollmentBinding(&$query, $values) {
+	$query->bindParam(':user_id', $values['user_id']);
+	$query->bindParam(':player_id', $values['player_id']);
+	$query->bindParam(':team_id', $values['team_id']);
+	$query->bindParam(':league_id', $values['league_id']);
+	$query->bindParam(':enrollment_date', $values['enrollment_date']);
+	$query->bindParam(':fee', $values['fee']);
+}
+
+/**
+ * Binds the values needed to insert a place.
+ *
+ * @param unknown $query
+ * @param unknown $values
+ */
+function addPlaceBinding(&$query, $values) {
+	$query->bindParam(':place_name', $values['place_name']);
+	$query->bindParam(':street_address', $values['street_address']);
+	$query->bindParam(':city', $values['city']);
+	$query->bindParam(':state', $values['state']);
+	$query->bindParam(':zip', $values['zip']);
+	$query->bindParam(':fee', $values['fee']);
+}
+
+/**
+ * Binds the values needed to insert an event.
+ *
+ * @param unknown $query
+ * @param unknown $values
+ */
+function addEventBinding(&$query, $values) {
+	$query->bindParam(':event_type', $values['event_type']);
+	$query->bindParam(':start_date', $values['start_date']);
+	$query->bindParam(':place_id', $values['place_id']);
+	$query->bindParam(':home_team_id', $values['home_team_id']);
+	$query->bindParam(':away_team_id', $values['away_team_id']);
 }
 
 /**
@@ -82,25 +153,31 @@ function insertOperation($values) {
             switch($values['table_name']) {
             case 'SPORTS':
                 $query = $db->prepare(ADD_SPORT);
+                addSportBinding($query, $values);
                 break;
             case 'LEAGUE':
                 $query = $db->prepare(ADD_LEAGUE);
+                addLeagueBinding($query, $values);
                 break;
             case 'USERS':
                 $query = $db->prepare(ADD_USER);
-                insertUserBinding($query, $values);
+                addUserBinding($query, $values);
                 break;
             case 'TEAM':
                 $query = $db->prepare(ADD_TEAM);
+                addTeamBinding($query, $values);
                 break;
             case 'ENROLLMENT':
                 $query = $db->prepare(ADD_ENROLLMENT);
+                addEnrollmentBinding($query, $values);
                 break;
             case 'PLACE':
                 $query = $db->prepare(ADD_PLACE);
+                addPlaceBinding($query, $values);
                 break;
             case 'EVENTS':
                 $query = $db->prepare(ADD_EVENT);
+                addEventBinding($query, $values);
                 break;
             }
             if($query != null) {
@@ -130,11 +207,14 @@ function doPrepSelect($query) {
     return $query->fetchAll();
 }
 
-/* 
-    Switch on table name.
-    prepare the statement.
-    select and echo result in JSON
-*/
+/**
+ * Switch on table name.
+ * Prepare the statement.
+ * Do select operation.
+ * Echo the result in JSON format.
+ * 
+ * @param unknown $values
+ */
 function selectOperation($values) {
     //echo "begin select operation\r\n";
     global $db;
@@ -174,6 +254,20 @@ function selectOperation($values) {
     //echo "end select operation\r\n";
 }
 
+/**
+ * This gets what user type of user a given user_id is attached to
+ * based on their credentials.
+ * 
+ * 
+ * 
+ * THIS NEEDS TO BE TESTED!!!!!!
+ * 
+ * @param unknown $values
+ * @return The type of user
+ * 					ADMIN
+ * 					USER
+ * 					COACH
+ */
 function getUserType($values) {
     global $db;
     $user_type;
